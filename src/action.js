b60fe8a3c3d53c7d663de3597e6196a3f7d92c87
Allan Johnson <80973949+johnsoncwb@users.noreply.github.com>
2022-06-20 22:24:16 -0300

    change parameters

--- src/action.js
+++ src/action.js
@@ -8,10 +8,10 @@ async function run() {
   const { context = {} } = github;
   const { pull_request } = context.payload;
 
-  console.log("context", context);
+  console.log("context", pull_request.repository.owner);
 
   await octoKit.issues.createComment({
-    owner: pull_request.reposutory.owner.login,
+    owner: pull_request.repository.owner.login,
     repo: pull_request.name,
     issue_number: pull_request.number,
     body: "Thank yout for submit a new Pull Request! we will try to review it as soon as we can",
