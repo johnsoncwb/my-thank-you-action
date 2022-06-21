86224593522e341406a3c86be16d38e3a104b76f
Allan Johnson <80973949+johnsoncwb@users.noreply.github.com>
2022-06-20 21:58:39 -0300

    say thank you to new PRs

--- src/action.js
+++ src/action.js
@@ -1,5 +1,18 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
 async function run() {
-  console.log("hello world");
+  const githubToken = core.getInput("GITHUB_TOKEN");
+  const octoKit = github.getOctokit(githubToken);
+
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+
+  await github.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: "Thank yout for submit a new Pull Request! we will try to review it as soon as we can",
+  });
 }
 
 run();
