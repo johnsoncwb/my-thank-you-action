d483a07530a703bb8d2ac6b0ad7582da09377b44
Allan Johnson <80973949+johnsoncwb@users.noreply.github.com>
2022-06-20 22:26:34 -0300

    parameters

--- dist/index.js
+++ dist/index.js
@@ -9001,14 +9001,14 @@ async function run() {
   const { context = {} } = github;
   const { pull_request } = context.payload;
 
-  console.log("context", pull_request.repository.owner);
+  console.log("pull_request", pull_request);
 
-  await octoKit.issues.createComment({
-    owner: pull_request.repository.owner.login,
-    repo: pull_request.name,
-    issue_number: pull_request.number,
-    body: "Thank yout for submit a new Pull Request! we will try to review it as soon as we can",
-  });
+  // await octoKit.issues.createComment({
+  //   owner: pull_request.repository.owner.login,
+  //   repo: pull_request.name,
+  //   issue_number: pull_request.number,
+  //   body: "Thank yout for submit a new Pull Request! we will try to review it as soon as we can",
+  // });
 }
 
 run();
